e6390d246a90e5bc5bb7565ae389c12d82c57eec
MyeongJunKim <myeongjun222@naver.com>
2023-07-24 12:46:22 +0900

    bug: query parameter 변경 감지 누락

--- src/hooks/useUrlSearchParams.ts
+++ src/hooks/useUrlSearchParams.ts
@@ -1,17 +1,20 @@
 import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 type Props = {
   url: string;
 };
 
 function useUrlSearchParams({ url }: Props) {
+  const location = useLocation();
   const [queryParam, setQueryParam] = useState('');
 
   useEffect(() => {
-    const urlSearchParams = new URLSearchParams(window.location.search);
+    const urlSearchParams = new URLSearchParams(location.search);
+    const param = urlSearchParams.get(url) ?? '';
 
-    setQueryParam(urlSearchParams.get(url) ?? '');
-  }, []);
+    setQueryParam(param);
+  }, [location.search]);
 
   return {
     queryParam,
