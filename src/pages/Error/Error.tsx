f05ae543aec62ff0bf2d1c66ad6df5fbbdd87c36
MyeongJunKim <myeongjun222@naver.com>
2023-07-24 12:44:03 +0900

    feat: ErrorBoundary 설정 및 recoil 추가

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,9 +1,7 @@
-import classNames from 'classnames/bind';
 import style from './style.module.scss';
-const cx = classNames.bind(style);
 
 function Error() {
-  return <div className={cx('error')}>500 Error</div>;
+  return <div className={style.error}>500 Error</div>;
 }
 
 export default Error;
